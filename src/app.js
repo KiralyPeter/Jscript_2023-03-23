843d9e767e9f47c23710183d8a77ab786e3c2e56
Peter Kiraly <peter.kiral@gmail.com>
2023-03-23 18:08:46 +0100

    index.html + app.js init

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
 // init
+
+const doc = {
+     tbody: document.querySelector("#tbody")
+};
+
+const state = {
+    host: "http://localhost:8000/",
+    todos: []
+};
+
+getTodos();
+
+function getTodos(){
+    // host + végpont
+    let url = state.host + "todos"
+    fetch(url) // fetch - asszinkron utasítás...
+    .then(response => response.json())
+    .then(result => {
+        console.log(result);
+    }); 
+}
